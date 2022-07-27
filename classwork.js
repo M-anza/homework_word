71b4a874c381ebce831e098d302cd723ac2c881c
M-anza <magomaanza@gmail.com>
2022-07-27 18:00:45 +0200

    classwork added

---
+++ classwork.js
@@ -0,0 +1,28 @@
+// function add(num1, num2){
+//     let results = num1  / num2
+// console.log(results);
+// }
+
+// add(5, 7)
+
+
+function addition(start, end){
+    for(let i = 1; i <= 20; i++){
+        if(start => 1){
+            console.log(i)
+        }else if(end <= 20){
+            break
+        }
+    }  
+}
+addition(1, 20)
+
+// function  twoInt(num1, num2){
+//     let total = num1 + num2;
+//     if(total > 15){
+//         return true;
+//     }else{
+//        return false;
+//     }
+// }
+// console.log(twoInt(10, 5))
